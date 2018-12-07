d71acf86d3b01f8bebfdd75b3a54f0087dddefac
Peter Halliday <pghalliday@gmail.com>
2018-12-07 10:37:38 +0100

    don't forget to convert strings to numbers

--- js/control.js
+++ js/control.js
@@ -1,48 +1,48 @@
 var stepAngleInput = document.querySelector('#stepAngle');
 stepAngleInput.value = toDegrees(STEP_ANGLE);
 stepAngleInput.onchange = function(event) {
-  STEP_ANGLE = toRadians(event.target.value);
+  STEP_ANGLE = toRadians(parseFloat(event.target.value));
   updateScene();
 };
 
 var stairsOffsetAngleInput = document.querySelector('#stairsOffsetAngle');
 stairsOffsetAngleInput.value = toDegrees(STAIRS_OFFSET_ANGLE);
 stairsOffsetAngleInput.onchange = function(event) {
-  STAIRS_OFFSET_ANGLE = toRadians(event.target.value);
+  STAIRS_OFFSET_ANGLE = toRadians(parseFloat(event.target.value));
   updateScene();
 };
 
 var bordesAngleInput = document.querySelector('#bordesAngle')
 bordesAngleInput.value = toDegrees(BORDES_ANGLE);
 bordesAngleInput.onchange = function(event) {
-  BORDES_ANGLE = toRadians(event.target.value);
+  BORDES_ANGLE = toRadians(parseFloat(event.target.value));
   updateScene();
 };
 
 var doorWidthInput = document.querySelector('#doorWidth');
 doorWidthInput.value = DOOR_WIDTH;
 doorWidthInput.onchange = function(event) {
-  DOOR_WIDTH = event.target.value;
+  DOOR_WIDTH = parseFloat(event.target.value);
   updateScene();
 };
 
 doorRightOffsetInput = document.querySelector('#doorRightOffset');
 doorRightOffsetInput.value = DOOR_RIGHT_OFFSET;
 doorRightOffsetInput.onchange = function(event) {
-  DOOR_RIGHT_OFFSET = event.target.value;
+  DOOR_RIGHT_OFFSET = parseFloat(event.target.value);
   updateScene();
 };
 
 var radiusInput = document.querySelector('#radius')
 radiusInput.value = STEP_RADIUS;
 radiusInput.onchange = function(event) {
-  STEP_RADIUS = event.target.value;
+  STEP_RADIUS = parseFloat(event.target.value);
   updateScene();
 };
 
 var wallGapInput = document.querySelector('#wallGap')
 wallGapInput.value = WALL_GAP;
 wallGapInput.onchange = function(event) {
-  WALL_GAP = event.target.value;
+  WALL_GAP = parseFloat(event.target.value);
   updateScene();
 };
